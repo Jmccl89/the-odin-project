10a0d867ac16de12de4eef01f7b3f6646986d202
James McCloskey <j.mccloskey89@gmail.com>
2015-12-22 19:07:50 +0000

    initial commit of caesar_cypher project

---
+++ ruby-programming/caesar-cypher.rb
@@ -0,0 +1,31 @@
+# Shift each alpha character by specified value maintaining case and 
+# ensuring Z-A and z-a rollover. Do not shift any non-alpha characters.
+#
+#    caesar_cypher("Test.", 4)              => "Xiwx."
+#    caesar_cypher("Test.", 10)             => "Docd."
+#    caesar_cypher("Test.", -10)            => "Juij."
+#    caesar_cypher("Sentence, test!", 5)    => "Xjsyjshj, yjxy!"
+
+def caesar_cypher(string, shift)
+    new_string = ""
+    string.split("").each do |x| 
+        if x.ord >= 65 && x.ord <= 90
+            y = x.ord + shift
+            y -= 26 while y > 90
+            y += 26 while y < 65
+        elsif x.ord >= 97 && x.ord <= 122
+            y = x.ord + shift
+            y -= 26 while y > 122
+            y += 26 while y < 97
+        else
+            y = x.ord
+        end
+        new_string << y.chr
+    end
+    new_string
+end
+
+puts caesar_cypher("Test.", 4)
+puts caesar_cypher("Test.", 10)
+puts caesar_cypher("Test.", -10)
+puts caesar_cypher("Sentence, test!", 5)
