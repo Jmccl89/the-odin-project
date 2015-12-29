b60569c2d35b84a87ce16dd185d12808445a7bc1
James McCloskey <james@mccloskey.io>
2015-12-28 18:23:21 -0600

    initial commit of bubble_sort

---
+++ ruby-programming/bubble_sort.rb
@@ -0,0 +1,21 @@
+def bubble_sort(list)
+	counter = list.length-1
+	iterations = 0
+	while counter > 1 do
+		for i in 0..counter-1
+				first = list[i]
+				second = list[i+1]
+				if first > second
+					list[i] = second
+					list[i+1] = first
+				end
+				iterations += 1
+		end
+		counter -= 1
+	end
+	puts "Elements: #{list.length}"
+	puts "Iterations: #{iterations}"
+	return list
+end
+
+puts bubble_sort([2,4,1,5,6,1])
